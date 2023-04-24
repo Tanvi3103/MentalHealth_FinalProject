d2c6f46ee93c56bd918c23eccbca92d2f78c7f3f
IshaJ-Git <66546632+IshaJ-Git@users.noreply.github.com>
2023-04-24 18:02:23 +0530

    changes in app.py, main.css

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 from flask import Flask, render_template,request
-from flask import jsonify
+#from flask import jsonify
 import sqlite3 as lite 
 import pickle
+from flask_sqlalchemy import SQLAlchemy
 
 model=pickle.load(open('pickle_model.pkl','rb'))
 # Set up Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
+
+class test(db.Model):
+    gender=db.column(db.String(100))
+
+    def __repr__(self):
+        return '<task %r>' % self.gender
+
 
 # Set up app routes
 @app.route("/")
 def index():
-    #return render_template("index.html")
-    return "Hello flask"
+    return render_template("index.html")
 
 @app.route('/Take_a_Test',methods=['POST','GET'])
 def Take_a_Test():
     return render_template('test.html')
 
-# @app.route('/api/v1.0/data', methods=['GET'])
-# def get_data():
-#     con = lite.connect('mental_health.db')
-#     with con:
-#         cur = con.cursor()
-#         cur.execute("SELECT * FROM pre_encoded_questions")
-#         rows = cur.fetchall()
-#         result = []
-#         for row in rows:
-#             result.append(row)
-#     return jsonify({'data': result})
 
 # Tell Flask to run
 if __name__ == "__main__":
